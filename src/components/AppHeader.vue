c54c64def7aeff126cdd0bbc42bb0299920063dc
codingcele <marcocelegato9@gmail.com>
2022-12-21 23:54:51 +0100

    header + over con emit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,124 @@
+<script>
+
+import { store } from "../store.js";
+
+export default {
+    name: "AppHeader",
+    data() {
+        return {
+            store,
+        }
+    },
+    methods: {
+        submit() {
+            this.$emit('changeColor')
+        }
+    },
+}
+
+</script>
+
+
+<template>
+    <header>
+        <div class="top">
+            <img :src=store.Header.img alt="">
+            <div class="menu">
+                <font-awesome-icon :icon=store.Header.fa />
+            </div>
+        </div>
+        <div class="center">
+            <h1 class="untold">
+                Untold Stories
+            </h1>
+            <span class="phrase">
+                There is an untold story behind every favorite song.
+            </span>
+            <div class="buttons">
+                <div class="button latest" :class="{ change: store.Header.BtnLatest }" @mouseover="$emit('changeColor')"
+                    @mouseout="$emit('default')">
+                    LATEST ALBUM
+                </div>
+                <div class="button live">
+                    LIVE DATES
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+
+<style lang = "scss" scoped>
+header {
+    height: 110vh;
+    background-image: url("../assets/img/home_slider.jpg");
+    background-size: cover;
+    background-position-y: -50px;
+
+    .top {
+        display: flex;
+        justify-content: space-between;
+        padding: 40px;
+
+        img {
+            cursor: pointer;
+        }
+
+        .menu {
+            display: flex;
+            align-items: center;
+            color: white;
+            font-size: 30px;
+            cursor: pointer;
+        }
+    }
+
+    .center {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: calc(100% - 155px);
+
+        .untold {
+            color: white;
+            font-size: 80px;
+        }
+
+        .phrase {
+            color: white;
+            padding: 15px;
+            font-family: serif;
+            font-size: 18px;
+        }
+
+        .buttons {
+            display: flex;
+            padding: 20px;
+
+            .button {
+                margin: 7px;
+                padding: 10px 25px;
+                border-radius: 2px;
+                text-align: center;
+                font-size: 12px;
+                color: white;
+                cursor: pointer;
+            }
+
+            .latest {
+                background-color: rgb(234, 74, 86);
+            }
+
+            .live {
+                border: 1px solid white;
+            }
+
+            .change {
+                color: black;
+                background-color: white;
+            }
+        }
+    }
+}
+</style>
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    Header: {
+        img: "/avada-music-logo.png",
+        fa: "fa-solid fa-bars",
+        BtnLatest: false,
+    }
+
+})
